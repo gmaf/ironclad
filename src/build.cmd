9912f1441752869e5809b203b995ee0f2b9db663
Cameron Fletcher <cameron.fletcher@cameronfletcher.com>
2018-01-09 20:18:52 +0100

    AUTH-15 Almost there with the build script...

--- src/build.cmd
+++ src/build.cmd
@@ -1,1 +1,9 @@
-docker build .
+docker rmi ironclad -f
+docker rmi ironclad_nginx -f
+
+docker rm $(docker ps -aq) -f
+docker network prune
+
+docker-compose -f Docker\docker-compose.yml -f Docker\docker-compose.release.yml -p ironclad config -e version=0.0.2
+docker-compose -f Docker\docker-compose.yml -f Docker\docker-compose.release.yml -p ironclad build -e version=0.0.2
+docker-compose -f Docker\docker-compose.yml -f Docker\docker-compose.release.yml -p ironclad up -d --force-recreate --remove-orphans
