443ef86f58df94e92d93d6bf46412a52c09603af
Cameron Fletcher <cameron.fletcher@cameronfletcher.com>
2018-01-09 10:33:58 +0100

    AUTH-16 Created configuration settings for different environments

--- src/Ironclad/Program.cs
+++ src/Ironclad/Program.cs
@@ -4,13 +4,32 @@
 
 namespace Ironclad
 {
+    using System;
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
 
     public static class Program
     {
         public static void Main(string[] args) => BuildWebHost(args).Run();
 
-        public static IWebHost BuildWebHost(string[] args) => WebHost.CreateDefaultBuilder(args).UseStartup<Startup>().Build();
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            // HACK (Cameron): Currently, there is no nice way to get a handle on IHostingEnvironment inside of Main() so we work around this...
+            // LINK (Cameron): https://github.com/aspnet/KestrelHttpServer/issues/1334
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var container = Environment.GetEnvironmentVariable("CONTAINER");
+
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
+                .AddCommandLine(args)
+                .Build();
+
+            return WebHost.CreateDefaultBuilder(args)
+                .UseConfiguration(configuration)
+                .UseStartup<Startup>()
+                .Build();
+        }
     }
 }
--- src/Ironclad/Startup.cs
+++ src/Ironclad/Startup.cs
@@ -7,15 +7,16 @@ namespace Ironclad
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Http;
-    ////using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.AspNetCore.HttpOverrides;
+    using Microsoft.Extensions.DependencyInjection;
 
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        ////public void ConfigureServices(IServiceCollection services)
-        ////{
-        ////}
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -24,6 +25,8 @@ namespace Ironclad
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions { ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto });
 
             app.Run(async (context) =>
             {
