39b7b2922fb4f1e80bd195d1a2a84e6a4294df96
waqas zamir <waqas.zamir@lykke.com>
2018-01-18 18:04:24 +0500

    AUTH-27 added information logs in PostgresDatabase Seeder

--- src/Ironclad/Configurations/PostgresDatabaseSeeder.cs
+++ src/Ironclad/Configurations/PostgresDatabaseSeeder.cs
@@ -4,6 +4,7 @@
     using IdentityServer4.Postgresql.Mappers;
     using Marten;
     using Microsoft.AspNetCore.Builder;
+    using Serilog;
 
     public class PostgresDatabaseSeeder : IDatabaseSeeder
     {
@@ -16,27 +17,43 @@
 
         public void Seed(IApplicationBuilder app)
         {
+            Log.Information($"Seeding postgres database: {this.config.Database}");
+
             var store = DocumentStore.For(this.config.ToString());
+
+            Log.Information($"Clearing all data from database: {this.config.Database}");
+
             store.Advanced.Clean.CompletelyRemoveAll();
+
             using (var session = store.LightweightSession())
             {
                 if (!session.Query<IdentityServer4.Postgresql.Entities.Client>().Any())
                 {
+                    Log.Information("Adding clients");
+
                     session.StoreObjects(Config.GetInMemoryClients().Select(c => c.ToEntity()));
                 }
 
                 if (!session.Query<IdentityServer4.Postgresql.Entities.ApiResource>().Any())
                 {
+                    Log.Information("Adding ApiResources");
+
                     session.StoreObjects(Config.GetApiResources().Select(r => r.ToEntity()));
                 }
 
                 if (!session.Query<IdentityServer4.Postgresql.Entities.IdentityResource>().Any())
                 {
+                    Log.Information("Adding IdentityResources");
+
                     session.StoreObjects(Config.GetIdentityResources().Select(r => r.ToEntity()));
                 }
 
                 session.SaveChanges();
+
+                Log.Information($"Saving all changes to database: {this.config.Database}");
             }
+
+            Log.Information($"Seed Completed");
         }
     }
 }
