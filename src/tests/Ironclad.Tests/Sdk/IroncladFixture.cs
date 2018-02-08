38026a7cd07a759242ed9a3f240365a01783f1d6
Cameron Fletcher <cameron.fletcher@cameronfletcher.com>
2018-02-08 11:54:45 +0100

    AUTH-32 Configured postgres docker container for tests

--- src/tests/Ironclad.Tests/Sdk/IroncladFixture.cs
+++ src/tests/Ironclad.Tests/Sdk/IroncladFixture.cs
@@ -5,30 +5,48 @@ namespace Ironclad.Tests.Sdk
 {
     using System;
     using System.Diagnostics;
+    using System.Globalization;
     using System.Threading;
 
     public sealed class IroncladFixture : IDisposable
     {
-        private readonly Process process;
+        private static readonly string DockerContainerId = Guid.NewGuid().ToString("N", CultureInfo.InvariantCulture).Substring(12);
+
+        private readonly Process postgresProcess;
+        private readonly Process ironcladProcess;
 
         public IroncladFixture()
         {
-            // TODO (Cameron): Configure new Postgres database (container?)
-            this.process = Process.Start(
+            this.postgresProcess = Process.Start(
+                new ProcessStartInfo("docker", $"run --name {DockerContainerId} -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=ironclad -p 5432:5432 postgres:10.1-alpine")
+                {
+                    UseShellExecute = true,
+                });
+
+            // TODO (Cameron): This should be configurable.
+            Thread.Sleep(5000);
+
+            this.ironcladProcess = Process.Start(
                 new ProcessStartInfo("dotnet", @"..\..\..\..\..\Ironclad\bin\Debug\netcoreapp2.0\Ironclad.dll")
                 {
                     UseShellExecute = true,
                 });
 
-            // NOTE (Cameron): We need to give time to the process to start.
             // TODO (Cameron): This should be configurable.
-            Thread.Sleep(1000);
+            Thread.Sleep(5000);
         }
 
         public void Dispose()
         {
-            this.process.Kill();
-            this.process.Dispose();
+            this.ironcladProcess.Kill();
+            this.ironcladProcess.Dispose();
+
+            this.postgresProcess.Kill();
+            this.postgresProcess.Dispose();
+
+            // NOTE (Cameron): Remove the docker container.
+            Process.Start(new ProcessStartInfo("docker", $"rm {DockerContainerId} -f"))
+                .WaitForExit(5000);
         }
     }
 }
