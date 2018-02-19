393e05aab9af250a15e10460619af129b3105501
Cameron Fletcher <cameron.fletcher@cameronfletcher.com>
2018-02-19 20:36:02 +0100

    AUTH-90 Bug fix for misleading error message on user create with roles

---
+++ src/tests/Ironclad.Tests/Bug/Bug00090.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Lykke Corp.
+// See the LICENSE file in the project root for more information.
+
+namespace Ironclad.Tests.Bug
+{
+    using System;
+    using System.Globalization;
+    using System.Threading.Tasks;
+    using FluentAssertions;
+    using Ironclad.Client;
+    using Ironclad.Tests.Sdk;
+    using Xunit;
+
+    public class Bug00090 : IntegrationTest
+    {
+        public Bug00090(IroncladFixture fixture)
+            : base(fixture)
+        {
+        }
+
+        [Fact]
+        public void ShouldNotThrowInternalServerError()
+        {
+            // arrange
+            var httpClient = new UsersHttpClient(this.Authority, this.Handler);
+            var user = new User
+            {
+                Username = Guid.NewGuid().ToString("N", CultureInfo.InvariantCulture),
+                Password = "password",
+                Email = "test@test.com",
+                PhoneNumber = "0123456789",
+                Roles = { "auth_admin", "user_admin" },
+            };
+
+            // act
+            Func<Task> func = async () => await httpClient.AddUserAsync(user).ConfigureAwait(false);
+
+            // assert
+            func.Should().NotThrow<HttpException>();
+        }
+    }
+}
