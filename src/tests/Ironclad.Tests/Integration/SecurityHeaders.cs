084f4426a7ab892d43ed8ed0be239903313e52a9
pawelros <pawel.rosinski@lykke.com>
2018-11-27 10:26:28 +0100

    DH-40 Fix security headers tests

--- src/tests/Ironclad.Tests/Integration/SecurityHeaders.cs
+++ src/tests/Ironclad.Tests/Integration/SecurityHeaders.cs
@@ -21,9 +21,9 @@ namespace Ironclad.Tests.Feature
         [InlineData("X-Frame-Options", "SAMEORIGIN")]
         [InlineData("Referrer-Policy", "no-referrer")]
         [InlineData("X-XSS-Protection", "1; mode=block")]
-        [InlineData("Content-Security-Policy", "default-src 'self'; object-src 'none'; frame-ancestors 'none'; sandbox allow-forms allow-same-origin allow-scripts; base-uri 'self';")]
-        [InlineData("X-Content-Security-Policy", "default-src 'self'; object-src 'none'; frame-ancestors 'none'; sandbox allow-forms allow-same-origin allow-scripts; base-uri 'self';")]
-        [InlineData("Feature-Policy", "geolocation 'none'; midi 'none'; notifications 'self'; push 'self'; microphone 'none'; camera 'none'; magnetometer 'none'; gyroscope 'none'; speaker 'none'; vibrate 'none'; payment 'none'")]
+        [InlineData("Content-Security-Policy", "script-src 'self' 'sha256-SjXRkVC/0M0+WLq2GU4E8JdbZ/ZNgspoHSzWQaMhG7E=' https://az416426.vo.msecnd.net/scripts/a/ai.0.js; img-src 'self'; style-src 'self'; object-src 'none'; frame-ancestors 'none'; sandbox allow-forms allow-same-origin allow-scripts; base-uri 'self';")]
+        [InlineData("X-Content-Security-Policy", "script-src 'self' 'sha256-SjXRkVC/0M0+WLq2GU4E8JdbZ/ZNgspoHSzWQaMhG7E=' https://az416426.vo.msecnd.net/scripts/a/ai.0.js; img-src 'self'; style-src 'self'; object-src 'none'; frame-ancestors 'none'; sandbox allow-forms allow-same-origin allow-scripts; base-uri 'self';")]
+        [InlineData("Feature-Policy", "geolocation 'none'; microphone 'none'; camera 'none'")]
         public async Task ShouldConformToWwwSecurityHeadersIO(string header, string expectedValue)
         {
             /* https://securityheaders.io */
