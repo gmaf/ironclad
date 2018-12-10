ab1fe3bdcc157d6abfeb93a3321a669839cff6ea
Cameron Fletcher <cameron.fletcher@cameronfletcher.com>
2018-12-10 08:48:12 +0100

    IRON-8 Added nuget package publishing to the build

--- src/build/build.cs
+++ src/build/build.cs
@@ -10,6 +10,7 @@
 
     internal static class Program
     {
+        private const string DetectEnvironment      = "env";
         private const string RestoreNugetPackages   = "restore";
         private const string BuildSolution          = "build";
         private const string BuildDockerImage       = "docker";
@@ -26,69 +27,138 @@
             var configuration = new ConfigurationBuilder().AddEnvironmentVariables().Build();
             var settings = configuration.Get<Settings>();
 
-            Target(
-                    RestoreNugetPackages,                           () => Run("dotnet", "restore src/Ironclad.sln"));
+            var isPullRequest = default(bool);
+
+            var nugetServer = default(string);
+            var nugetApiKey = default(string);
+
+            var dockerRegistry = default(string);
+            var dockerUsername = default(string);
+            var dockerPassword = default(string);
+            var dockerTag = default(string);
 
             Target(
-                    BuildSolution,
-                    DependsOn(RestoreNugetPackages),                () => Run("dotnet", "build src/Ironclad.sln -c CI --no-restore"));
+                DetectEnvironment,
+                () =>
+                {
+                    // LINK (Cameron): https://docs.travis-ci.com/user/environment-variables/#default-environment-variables
+                    if (settings.TRAVIS == true)
+                    {
+                        Console.WriteLine("Travis build server detected.");
+
+                        if (int.TryParse(settings.TRAVIS_PULL_REQUEST, out var _))
+                        {
+                            Console.WriteLine("Pull request build detected.");
+                            isPullRequest = true;
+                            return;
+                        }
+
+                        if (!string.IsNullOrEmpty(settings.TRAVIS_TAG))
+                        {
+                            Console.WriteLine("Release build detected.");
+
+                            nugetServer = settings.BUILD_SERVER__NUGET__SERVER;
+                            nugetApiKey = settings.BUILD_SERVER__NUGET__API_KEY;
+
+                            dockerRegistry = settings.BUILD_SERVER__DOCKER_REGISTRY;
+                            dockerUsername = settings.BUILD_SERVER__DOCKER_USERNAME;
+                            dockerPassword = settings.BUILD_SERVER__DOCKER_PASSWORD;
+                            dockerTag = settings.TRAVIS_TAG;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Pre-release build detected.");
+
+                            nugetServer = settings.BUILD_SERVER__NUGET__BETA_SERVER;
+                            nugetApiKey = settings.BUILD_SERVER__NUGET__BETA_API_KEY;
+
+                            dockerRegistry = settings.BUILD_SERVER__DOCKER__BETA_REGISTRY;
+                            dockerUsername = settings.BUILD_SERVER__DOCKER__BETA_USERNAME;
+                            dockerPassword = settings.BUILD_SERVER__DOCKER__BETA_PASSWORD;
+                            dockerTag = "latest";
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Build server not detected.");
+                    }
+                });
 
             Target(
-                    BuildDockerImage,                               () => Run("docker", "build --tag ironclad ."));
+                RestoreNugetPackages,
+                () => Run("dotnet", "restore src/Ironclad.sln"));
 
             Target(
-                    TestDockerImage,                                // dotnet test --test-adapter-path:C:\Users\cameronfletcher\.nuget\packages\xunitxml.testlogger\2.0.0\build\_common --logger:"xunit;LogFilePath=test_result.xml"
-                    DependsOn(BuildSolution, BuildDockerImage),     () => Run("dotnet", $"test src/tests/Ironclad.Tests/Ironclad.Tests.csproj -r ../../../{ArtifactsFolder} -l trx;LogFileName=Ironclad.Tests.xml --no-build"));
+                BuildSolution,
+                DependsOn(RestoreNugetPackages),
+                () => Run("dotnet", "build src/Ironclad.sln -c CI --no-restore"));
 
             Target(
-                    CreateNugetPackages,
-                    DependsOn(BuildSolution),                       ForEach("src/Ironclad.Client/Ironclad.Client.csproj", "src/Ironclad.Console/Ironclad.Console.csproj", "src/tests/Ironclad.Tests.Sdk/Ironclad.Tests.Sdk.csproj"),
-                                                                    project => Run("dotnet", $"pack {project} -c Release -o ../../{(project.StartsWith("src/tests") ? "../" : "") + ArtifactsFolder} --no-build"));
+                BuildDockerImage,
+                () => Run("docker", "build --tag ironclad ."));
 
             Target(
-                    PublishNugetPackages,
-                    DependsOn(CreateNugetPackages, TestDockerImage),() =>
-                                                                    {
-                                                                        var packagesToPublish = Directory.GetFiles(ArtifactsFolder, "*.nupkg", SearchOption.TopDirectoryOnly);
-                                                                        Console.WriteLine($"Found packages to publish: {string.Join("; ", packagesToPublish)}");
-
-                                                                        var apiKey = settings.NUGET__API_KEY;
-
-                                                                        if (string.IsNullOrWhiteSpace(apiKey))
-                                                                        {
-                                                                            Console.WriteLine("NuGet API key not specified. Packages will not be published.");
-                                                                            Console.WriteLine($"To enable publishing of NuGet packages please add the environment variables: '{nameof(Settings.NUGET__API_KEY)}' and '{nameof(Settings.NUGET__SERVER)}'.");
-                                                                            return;
-                                                                        }
-
-                                                                        foreach (var packageToPublish in packagesToPublish)
-                                                                        {
-                                                                            Run("dotnet", $"nuget push {packageToPublish} -s {settings.NUGET__SERVER} -k {apiKey}", noEcho: true);
-                                                                        }
-                                                                    });
+                TestDockerImage,
+                DependsOn(BuildSolution, BuildDockerImage),
+                // dotnet test --test-adapter-path:C:\Users\cameronfletcher\.nuget\packages\xunitxml.testlogger\2.0.0\build\_common --logger:"xunit;LogFilePath=test_result.xml"
+                () => Run("dotnet", $"test src/tests/Ironclad.Tests/Ironclad.Tests.csproj -r ../../../{ArtifactsFolder} -l trx;LogFileName=Ironclad.Tests.xml --no-build"));
 
             Target(
-                    PublishDockerImage,
-                    DependsOn(TestDockerImage),                     () =>
-                                                                    {
-                                                                        if (string.IsNullOrWhiteSpace(settings.BUILD_SERVER__DOCKER_REGISTRY))
-                                                                        {
-                                                                            Console.WriteLine("Docker registry not specified. Docker images will not be published.");
-                                                                            Console.WriteLine($"To enable publishing of docker images please add the environment variable: '{nameof(Settings.BUILD_SERVER__DOCKER_REGISTRY)}'.");
-                                                                            return;
-                                                                        }
+                CreateNugetPackages,
+                DependsOn(BuildSolution),
+                ForEach(
+                    "src/Ironclad.Client/Ironclad.Client.csproj", 
+                    "src/Ironclad.Console/Ironclad.Console.csproj", 
+                    "src/tests/Ironclad.Tests.Sdk/Ironclad.Tests.Sdk.csproj"),
+                project => Run("dotnet", $"pack {project} -c Release -o ../../{(project.StartsWith("src/tests") ? "../" : "") + ArtifactsFolder} --no-build"));
 
-                                                                        if (string.IsNullOrWhiteSpace(settings.BUILD_SERVER__DOCKER_USERNAME) || string.IsNullOrWhiteSpace(settings.BUILD_SERVER__DOCKER_PASSWORD))
-                                                                        {
-                                                                            Console.WriteLine("Docker credentials not specified. Docker images will not be published.");
-                                                                            Console.WriteLine($"To enable publishing of docker images please add the environment variables: '{nameof(Settings.BUILD_SERVER__DOCKER_USERNAME)}' and '{nameof(Settings.BUILD_SERVER__DOCKER_PASSWORD)}'.");
-                                                                            return;
-                                                                        }
+            Target(
+                PublishNugetPackages,
+                DependsOn(DetectEnvironment, CreateNugetPackages, TestDockerImage),
+                () =>
+                {
+                    var packagesToPublish = Directory.GetFiles(ArtifactsFolder, "*.nupkg", SearchOption.TopDirectoryOnly);
+                    Console.WriteLine($"Found packages to publish: {string.Join("; ", packagesToPublish)}");
 
-                                                                        Run("docker", $"login {settings.BUILD_SERVER__DOCKER_REGISTRY} -u {settings.BUILD_SERVER__DOCKER_USERNAME} -p {settings.BUILD_SERVER__DOCKER_PASSWORD}");
-                                                                        Run("docker", $"tag ironclad:latest {settings.BUILD_SERVER__DOCKER_REGISTRY}/ironclad:latest");
-                                                                        Run("docker", $"docker push {settings.BUILD_SERVER__DOCKER_REGISTRY}/ironclad:latest");
-                                                                    });
+                    if (isPullRequest)
+                    {
+                        Console.WriteLine("Build is pull request. Packages will not be published.");
+                        return;
+                    }
+
+                    if (string.IsNullOrWhiteSpace(nugetServer) || string.IsNullOrWhiteSpace(nugetApiKey))
+                    {
+                        Console.WriteLine("NuGet settings not specified. Packages will not be published.");
+                        return;
+                    }
+
+                    foreach (var packageToPublish in packagesToPublish)
+                    {
+                        Run("dotnet", $"nuget push {packageToPublish} -s {nugetServer} -k {nugetApiKey}", noEcho: true);
+                    }
+                });
+
+            Target(
+                PublishDockerImage,
+                DependsOn(DetectEnvironment, TestDockerImage),
+                () =>
+                {
+                    if (isPullRequest)
+                    {
+                        Console.WriteLine("Build is pull request. Docker images will not be published.");
+                        return;
+                    }
+
+                    if (string.IsNullOrWhiteSpace(dockerRegistry) || string.IsNullOrWhiteSpace(dockerUsername) || string.IsNullOrWhiteSpace(dockerPassword))
+                    {
+                        Console.WriteLine("Docker credentials not specified. Docker images will not be published.");
+                        return;
+                    }
+
+                    Run("docker", $"login {dockerRegistry} -u {dockerUsername} -p {dockerPassword}");
+                    Run("docker", $"tag ironclad:latest {dockerRegistry}/ironclad:{dockerTag}");
+                    Run("docker", $"docker push {dockerRegistry}/ironclad:{dockerTag}");
+                });
 
             Target(
                     PublishAll,
@@ -103,8 +173,19 @@
 
         private class Settings
         {
-            public string NUGET__SERVER { get; set; }
-            public string NUGET__API_KEY { get; set; }
+            // travis specific
+            public bool? TRAVIS { get; set; }
+            public string TRAVIS_PULL_REQUEST { get; set; }
+            public string TRAVIS_TAG { get; set; }
+
+            // build specific
+            public string BUILD_SERVER__NUGET__BETA_SERVER { get; set; }
+            public string BUILD_SERVER__NUGET__BETA_API_KEY { get; set; }
+            public string BUILD_SERVER__NUGET__SERVER { get; set; }
+            public string BUILD_SERVER__NUGET__API_KEY { get; set; }
+            public string BUILD_SERVER__DOCKER__BETA_REGISTRY { get; set; }
+            public string BUILD_SERVER__DOCKER__BETA_USERNAME { get; set; }
+            public string BUILD_SERVER__DOCKER__BETA_PASSWORD { get; set; }
             public string BUILD_SERVER__DOCKER_REGISTRY { get; set; }
             public string BUILD_SERVER__DOCKER_USERNAME { get; set; }
             public string BUILD_SERVER__DOCKER_PASSWORD { get; set; }
